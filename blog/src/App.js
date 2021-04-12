c85107ebe31c09bc8384e44a9f776c8a0841f88d
myj1217 <myj1217@naver.com>
2021-04-12 23:28:13 +0900

    Add like

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,7 +8,7 @@ function App() {
 
   // state 데이터, state 변경 함수
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '남양주 카페 추천']);
-
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   return (
@@ -15,7 +17,7 @@ function App() {
         <div>Dev Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
